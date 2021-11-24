c899ed7990739b0ef124edbe2dddc58766bd5bab
Marvin Yan <marvinsyan@gmail.com>
2021-11-24 17:30:45 -0500

    Update deployPriceConsumer to use ETH/USD price feed

--- scripts/deployPriceConsumer.js
+++ scripts/deployPriceConsumer.js
@@ -1,12 +1,12 @@
-const CHAINLINK_PRICE_FEED_RINKEBY =
-  "0xECe365B379E1dD183B20fc5f022230C044d51404";
+// const CHAINLINK_BTCUSD_RINKEBY = "0xECe365B379E1dD183B20fc5f022230C044d51404";
+const CHAINLINK_ETHUSD_RINKEBY = "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e";
 
 const main = async () => {
   const priceConsumerFactory = await hre.ethers.getContractFactory(
     "PriceConsumerV3"
   );
   const priceConsumer = await priceConsumerFactory.deploy(
-    CHAINLINK_PRICE_FEED_RINKEBY
+    CHAINLINK_ETHUSD_RINKEBY
   );
   await priceConsumer.deployed();
   console.log(`PriceConsumerV3 deployed to ${priceConsumer.address}`);
