1c09dea3402636c684c423d9e6e8c9469ad02fb5
Marvin Yan <marvinsyan@gmail.com>
2021-11-25 02:05:08 -0500

    Add getRoundData

--- contracts/PriceConsumerV3.sol
+++ contracts/PriceConsumerV3.sol
@@ -6,32 +6,46 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract PriceConsumerV3 {
     AggregatorV3Interface internal priceFeed;
 
-    /**
-     * Network: Kovan
-     * Aggregator: ETH/USD
-     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
-     */
-
-    /**
-     * Network: Rinkeby
-     * Aggregator: BTC/USD
-     * Address: 0xECe365B379E1dD183B20fc5f022230C044d51404
-     */
-    constructor(address _priceFeed) public {
-        priceFeed = AggregatorV3Interface(_priceFeed);
+    constructor() {
+        // Rinkeby ETH/USD price feed: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
     }
 
-    /**
-     * Returns the latest price
-     */
-    function getLatestPrice() public view returns (int256) {
+    function latestRoundData()
+        public
+        view
+        returns (
+            uint80, // roundId
+            int256, // price (divide by 10^8 to get USD)
+            uint256, // startedAt (unix timestamp)
+            uint256, // updatedAt (unix timestamp)
+            uint80 // answeredInRound
+        )
+    {
+        return priceFeed.latestRoundData();
+    }
+
+    function getRoundData(uint80 _roundId)
+        public
+        view
+        returns (
+            uint80,
+            int256,
+            uint256,
+            uint256,
+            uint80
+        )
+    {
         (
-            uint80 roundID,
+            uint80 roundId,
             int256 price,
             uint256 startedAt,
-            uint256 timeStamp,
+            uint256 updatedAt,
             uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
-        return price;
+        ) = priceFeed.getRoundData(_roundId);
+        require(updatedAt > 0, "Round not complete");
+        return (roundId, price, startedAt, updatedAt, answeredInRound);
     }
 }
